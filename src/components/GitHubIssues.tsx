0a197676d8f5837c0b954cb38b2c25ac62663016
m-qm <41054106+m-qm@users.noreply.github.com>
2023-12-16 18:53:19 +0000

    fix gql

--- src/components/GitHubIssues.tsx
+++ src/components/GitHubIssues.tsx
@@ -6,28 +6,12 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import GitHubIssuesFilter from './GitHubIssuesFilter';
 import GitHubIssuesTable from './GitHubIssuesTable';
 import GitHubIssuesPagination from './GitHubIssuesPagination';
-import { GitHubIssuesData, GitHubIssue, IssueState } from '../types';
-import { accessToken } from '../config';
+import { GitHubIssuesData } from '../types';
 
 const GITHUB_ISSUES_QUERY = gql`
-  query GetGitHubIssues($owner: String!, $repo: String!, $labels: [String!], $status: [IssueState!], $cursor: String) {
+  query GetGitHubIssues($owner: String!, $repo: String!) {
     repository(owner: $owner, name: $repo) {
-      openIssues: issues(first: 5, after: $cursor, states: $status, filterBy: { labels: $labels }) {
-        nodes {
-          id
-          title
-          labels(first: 5) {
-            nodes {
-              name
-            }
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-        }
-      }
-      closedIssues: issues(first: 5, after: $cursor, states: [CLOSED], filterBy: { labels: $labels }) {
+      issues(first: 5) {
         nodes {
           id
           title
@@ -54,37 +38,29 @@ const pieChartData = [
   { name: 'Label 5', value: 30, fill: '#AF19FF' },
 ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
-
 const GitHubIssues: React.FC = () => {
   const [labels, setLabels] = useState<string[]>([]);
-  const [status, setStatus] = useState<IssueState[]>(['OPEN']);
   const [cursor, setCursor] = useState<string | null>(null);
 
-  // add a try catch block to handle errors
   const { loading, error, data, fetchMore } = useQuery<GitHubIssuesData>(GITHUB_ISSUES_QUERY, {
-    variables: { owner: 'facebook', repo: 'react', labels, status, cursor },
-    context: {
-      headers: {
-        Authorization: `bearer ${accessToken}`,
-      },
-    },
+    variables: { owner: 'facebook', repo: 'docusaurus' },
   });
+
+  console.log('Data:', data);
 
   if (loading) return <p>Loading...</p>;
 
-  if (error) return <p>Error {error.message}</p>;
+  if (error) return <p>Error :(</p>;
 
-  const openIssues = data?.repository.openIssues.nodes || [];
-  const closedIssues = data?.repository.closedIssues.nodes || [];
-  const pageInfo = data?.repository.openIssues.pageInfo || {};
+  const openIssues = data?.repository?.issues.nodes || [];
+  const pageInfo = data?.repository?.issues.pageInfo || {};
 
-  const issues = openIssues; // Assuming you want to display open issues by default
+  const issues = openIssues;
 
   const handlePageChange = (newCursor: string) => {
     setCursor(newCursor);
     fetchMore({
-      variables: { owner: 'facebook', repo: 'react', labels, status, cursor: newCursor },
+      variables: { owner: 'facebook', repo: 'docusaurus', cursor: newCursor },
     });
   };
 
@@ -94,13 +70,12 @@ const GitHubIssues: React.FC = () => {
       <GitHubIssuesFilter
         labels={labels}
         setLabels={setLabels}
-        status={status}
-        setStatus={setStatus}
         setCursor={setCursor}
         fetchMore={fetchMore}
         owner="facebook"
-        repo="react"
+        repo="docusaurus"
       />
+      <GitHubIssuesTable issues={issues} />
       <ResponsiveContainer width="100%" height={400}>
         <PieChart>
           <Pie
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 // src/index.tsx (or the file where your app is initialized)
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './components/GitHubIssues';
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { accessToken } from './config';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql', // Update with your proxy server URL
-  cache: new InMemoryCache(),
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
   headers: {
     Authorization: `bearer ${accessToken}`,
   },
 });
+
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
